8803067bd95aff40dbc55456c3dbdc24ff6debd0
robOcity <robert.osterburg@gmail.com>
2019-11-21 20:34:45 -0700

    feat: reading geographic location codes json file

--- analysis.py
+++ analysis.py
@@ -25,7 +25,38 @@ def main():
     # total number of accidents
     print(f"Fatal Accidents 1982 to 1018: {accidents.count():,}")
 
+    # TODO cleanup after getting
+    # create dataframe of cities of interest
+    columns = ["City", "County", "State", "GNIS_ID"]
+    values = [
+        ("Denver", "Denver", "Colorado", "2410324"),
+        ("Seattle", "King", "Washington", "2411856"),
+    ]
+    city_df = spark.createDataFrame(values, columns)
+    city_df.show()
+
+    # read in geographic location codes as json
+    glc_path = str(
+        utils.get_external_data_path(
+            src_dir="FRPP_GLC", filename="FRPP_GLC_United_States.json"
+        )
+    )
+
+    # schema =
+    print(f"glc_path={glc_path}")
+    glc_df = spark.read.json(
+        glc_path, mode="FAILFAST", multiLine=True, allowNumericLeadingZero=True
+    )
+    glc_df.show(5)
+
     # accidents per year in denver, co and seattle, wa
+    spark.sql(
+        """
+    SELECT count(*) 
+    FROM accidents
+    WHERE STATE = 08 AND COUNTY = 031 AND CITY = 0600
+    """
+    ).show()
 
     # per capita accidents per year in denver, co and seattle, wa
 
