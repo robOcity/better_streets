a679576435b6693f27c9e91b51d91f352871dcfa
robOcity <robert.osterburg@gmail.com>
2020-01-02 11:10:19 -0700

    chore: add function doc

--- better_streets/utils.py
+++ better_streets/utils.py
@@ -12,12 +12,17 @@ def create_spark_session():
 
 
 def load_env():
+    """Loads key-value pairs from your .env file as system environment variables."""
+
     env_path = Path(".") / ".env"
     load_dotenv(dotenv_path=env_path, verbose=True)
 
     assert (
         os.getenv("DATA_ROOT") is not None
-    ), "Environment variable with the root data directory has not been set"
+    ), "Environment variable with the root data directory has not been set."
+    assert (
+        os.getenv("PROJECT_KEY") is not None
+    ), "Environment variable with the name of the project has not been set."
 
 
 def read_csv(path):
@@ -43,6 +48,8 @@ def write_csv(df, path):
 
 
 def get_dir(root, project, kind, source):
+    """Returns the data directory root for the project."""
+
     return Path(root).joinpath(project).joinpath(kind).joinpath(source)
 
 
