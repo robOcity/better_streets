3db4eea4c5970c5019bd17cf6ae78128fead25b0
Rob Osterburg <robert.osterburg@gmail.com>
2020-03-22 15:18:16 -0600

    chore: comments with results

--- peds/analysis.py
+++ peds/analysis.py
@@ -35,7 +35,6 @@ def main():
 
     # total number of accidents
     print(f"\nFatal Accidents 1982 to 1018: {accidents.count():,}")
-    # Fatal Accidents 1982 to 1018: 1,349,445
 
     # read in geographic location codes as json
     glc_path = utils.get_dir(root, project, "external", "FRPP_GLC").joinpath(
@@ -46,16 +45,6 @@ def main():
         str(glc_path), mode="FAILFAST", multiLine=True, allowNumericLeadingZero=True,
     )
     location.show(5)
-    # +---------+--------------+------------+-----------+-----------+-----------------+-------------+----------+----------+---------+
-    # |City_Code|     City_Name|Country_Code|County_Code|County_Name|Date_Record_Added|Old_City_Name|State_Code|State_Name|Territory|
-    # +---------+--------------+------------+-----------+-----------+-----------------+-------------+----------+----------+---------+
-    # |     0010|     ABBEVILLE|         840|        067|      HENRY|                 |             |        01|   ALABAMA|        U|
-    # |     0050|   ALBERTVILLE|         840|        095|   MARSHALL|                 |             |        01|   ALABAMA|        U|
-    # |     0060|ALEXANDER CITY|         840|        123| TALLAPOOSA|                 |             |        01|   ALABAMA|        U|
-    # |     0070|    ALICEVILLE|         840|        107|    PICKENS|                 |             |        01|   ALABAMA|        U|
-    # |     0090|     ANDALUSIA|         840|        039|  COVINGTON|                 |             |        01|   ALABAMA|        U|
-    # +---------+--------------+------------+-----------+-----------+-----------------+-------------+----------+----------+---------+
-    # only showing top 5 rows
 
     location.createOrReplaceTempView("location")
 
@@ -76,16 +65,6 @@ def main():
         """
     )
     den_sea_fatalities.show(5)
-    # +----+---------+--------------+
-    # |Year|City_Name|All_Fatalities|
-    # +----+---------+--------------+
-    # |1982|  SEATTLE|            53|
-    # |1982|   DENVER|            68|
-    # |1983|  SEATTLE|            50|
-    # |1983|   DENVER|            55|
-    # |1984|  SEATTLE|            45|
-    # +----+---------+--------------+
-    # only showing top 5 rows
 
     # save the results
     den_sea_fatalities_path = (
@@ -111,16 +90,6 @@ def main():
         """
     )
     den_sea_ped_bike_fatalities.show(5)
-    # +----+---------+-----------+
-    # |YEAR|City_Name|sum(FATALS)|
-    # +----+---------+-----------+
-    # |1982|  SEATTLE|         17|
-    # |1982|   DENVER|         24|
-    # |1983|  SEATTLE|         20|
-    # |1983|   DENVER|         13|
-    # |1984|  SEATTLE|         16|
-    # +----+---------+-----------+
-    # only showing top 5 rows
 
     # save the results
     den_sea_ped_bike_fatalities_path = utils.get_dir(
